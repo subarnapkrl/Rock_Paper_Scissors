d0c5db8828c90636e5dfc857a004147716b367cb
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-04-20 16:49:59 +0545

    Added-game-functionalities

---
+++ index.js
@@ -0,0 +1,52 @@
+function computerPlay(){
+    let computerChoice=Math.floor(Math.random()*3);
+    if(computerChoice==0){
+        return `rock`;
+    }
+    else if(computerChoice==1){
+        return `paper`;
+    }
+    else if(computerChoice==2){
+        return `scissors`;
+    }
+    else{
+        console.log("Invalid");
+    }
+}
+
+let compChoice=computerPlay();
+
+let userChoice=prompt("Please choose between Rock , Paper & Scissors:-");
+userChoice=userChoice.toLowerCase();
+console.log("You Choose:-"+userChoice);
+console.log("Computer Choose:-"+compChoice);
+
+
+function gamePlay(choiceUser,choiceComputer){
+  if(choiceUser==choiceComputer){
+      return `It's a TIE! YOU CHOSE ${choiceUser} and COMPUTER CHOSE ${choiceComputer}`;
+  }
+  else if(choiceUser=="rock" && choiceComputer=="paper"){
+      return `Computer Won! ${choiceComputer} beats ${choiceUser}`
+  }
+  else if(choiceUser=="rock" && choiceComputer=="scissors"){
+      return `You Won! ${choiceUser} beats ${choiceComputer}`
+  }
+  else if(choiceUser=="paper" && choiceComputer=="rock"){
+      return `You Won! ${choiceUser} beats ${choiceComputer}`
+  }
+  else if(choiceUser=="paper" && choiceComputer=="scissors"){
+      return `Computer Won! ${choiceComputer} beats ${choiceUser} `
+  }
+  else if(choiceUser=="scissors" && choiceComputer=="rock"){
+      return `Computer Won! ${choiceComputer} beats ${choiceUser}`
+  }
+  else if(choiceUser=="scissors" && choiceComputer=="paper"){
+      return `You Won! ${choiceUser} beats ${choiceComputer}`
+  }else{
+      return `Try Again!`
+  }
+
+}
+
+console.log(gamePlay(userChoice,compChoice));
