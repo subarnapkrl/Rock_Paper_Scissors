fc5bc63ab9f117cda162d7e5660e0ef9097fe092
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-04-21 17:02:12 +0545

    Added-some-comments

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Random Pick from Computer
+
 function computerPlay(){
     let computerChoice=Math.floor(Math.random()*3);
     if(computerChoice==0){
@@ -10,41 +12,44 @@ function computerPlay(){
         return `scissors`;
     }
     else{
-        console.log("Invalid");
+        console.log("Invalid ");
     }
 }
 
 let compChoice=computerPlay();
-
-let userChoice=prompt("Please choose between Rock , Paper & Scissors:-");
-userChoice=userChoice.toLowerCase();
-console.log("You Choose:-"+userChoice);
 console.log("Computer Choose:-"+compChoice);
 
 
+//Function to make user choose between Rock, Paper & SCissors
+let userChoice=prompt("Please choose between Rock , Paper & Scissors:-");
+userChoice=userChoice.toLowerCase();
+console.log("You Choose:-"+userChoice);
+
+
+//Gamplay Function to determine the winner
 function gamePlay(choiceUser,choiceComputer){
   if(choiceUser==choiceComputer){
       return `It's a TIE! YOU CHOSE ${choiceUser} and COMPUTER CHOSE ${choiceComputer}`;
   }
   else if(choiceUser=="rock" && choiceComputer=="paper"){
-      return `Computer Won! ${choiceComputer} beats ${choiceUser}`
+      return `Computer Won! ${choiceComputer} beats ${choiceUser}`;
   }
   else if(choiceUser=="rock" && choiceComputer=="scissors"){
-      return `You Won! ${choiceUser} beats ${choiceComputer}`
+      return `You Won! ${choiceUser} beats ${choiceComputer}`;
   }
   else if(choiceUser=="paper" && choiceComputer=="rock"){
-      return `You Won! ${choiceUser} beats ${choiceComputer}`
+      return `You Won! ${choiceUser} beats ${choiceComputer}`;
   }
   else if(choiceUser=="paper" && choiceComputer=="scissors"){
-      return `Computer Won! ${choiceComputer} beats ${choiceUser} `
+      return `Computer Won! ${choiceComputer} beats ${choiceUser} `;
   }
   else if(choiceUser=="scissors" && choiceComputer=="rock"){
-      return `Computer Won! ${choiceComputer} beats ${choiceUser}`
+      return `Computer Won! ${choiceComputer} beats ${choiceUser}`;
   }
   else if(choiceUser=="scissors" && choiceComputer=="paper"){
-      return `You Won! ${choiceUser} beats ${choiceComputer}`
+      return `You Won! ${choiceUser} beats ${choiceComputer}`;
   }else{
-      return `Try Again!`
+      return `Try Again!`;
   }
 
 }
